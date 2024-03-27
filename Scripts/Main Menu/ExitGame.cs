3ff35c9262ce291e19a9de4844bdc5cbf3e4a40b
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2024-03-27 09:00:22 +0200

    Add files via upload

---
+++ Scripts/Main Menu/ExitGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public bool rateGameOnExit;
+
+    public RateGame rateGame;
+
+    private bool windowOpened;
+    public GameObject panel_loading;
+
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        panel_loading.SetActive(true);
+        SceneManager.LoadSceneAsync(0 , LoadSceneMode.Single);
+    }
+
+    // Used to set the windowOpened value if other window is opened.
+    public void WindowOpened(bool opened)
+    {
+        windowOpened = opened;
+    }
+}
